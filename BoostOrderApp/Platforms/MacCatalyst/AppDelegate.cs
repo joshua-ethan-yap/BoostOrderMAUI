ff1c2d16fd6d3252bbbe5f21d0d91810fff7303e
Joshua Ethan Yap <joshua.ethan.yap2@gmail.com>
2025-09-04 09:29:55 +0800

    created initial repo

---
+++ BoostOrderApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BoostOrderApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
